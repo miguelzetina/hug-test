fc407f9ac682e8c0b21cb53c7858c6ceff7127f9
Miguel Zetina <loefthur26@gmail.com>
2019-02-20 22:34:27 -0600

    Expose API over HTTP

--- birthday_api.py
+++ birthday_api.py
@@ -1,9 +1,12 @@
 import hug
 
 
+@hug.get(examples='name=Mike&age=23')
 @hug.local()
 def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Say happy birthday to a user"""
     return {
         'message': 'Happy {0} Birthday {1}!'.format(age, name),
         'took': float(hug_timer)
     }
+
